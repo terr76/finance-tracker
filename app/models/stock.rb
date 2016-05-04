25020e66e8f7833f1254c091f574051ea8bc8279
Kibeom Kim <terr1976@hotmail.com>
2016-05-04 13:50:09 -0400

    add user and stock association and display

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,8 @@
 class Stock < ActiveRecord::Base
 
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
   end
@@ -17,8 +20,8 @@ class Stock < ActiveRecord::Base
     closing_price = StockQuote::Stock.quote(ticker).close
     return "#{closing_price} (Closing)" if closing_price
 
-    open_price = StockQuote::Stock.quote(ticker).open
-    return "#{opening_price} (Opening)" if closing_price
+    opening_price = StockQuote::Stock.quote(ticker).open
+    return "#{opening_price} (Opening)" if opening_price
     'Unavailable'
   end
 
